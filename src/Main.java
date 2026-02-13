4d31f94bd346e86773709abec0a077d30342d990
Evelyn perez uscanga <zs24016727@estudiantes.uv.mx>
2026-02-13 06:58:27 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+/*Escriba un programa Java para crear un método que tome un número
+entero como parámetro y produzca una excepción si el número es impar.*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un número entero:");
+        int numero = sc.nextInt();
+        sc.close();
+        try {
+            verificarPar(numero);
+            System.out.println("El número es par.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+//metodo
+    public static void verificarPar(int numero) {
+        if (numero % 2 != 0) {
+            throw new IllegalArgumentException("El número es impar.");
+        }
+    }
 }
